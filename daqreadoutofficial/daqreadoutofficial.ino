74c2cf098032fc54ca43ef8070e9055646d50513
izo0619 <isabelzhong30@gmail.com>
2021-01-16 13:55:55 -0800

    bug fixes and calibration implemented

--- daqreadoutofficial/daqreadoutofficial.ino
+++ daqreadoutofficial/daqreadoutofficial.ino
@@ -97,7 +97,9 @@ int PTUBE9_PIN = 83;
 int PTUBE10_PIN = 82;
 float PTUBE1, PTUBE2, PTUBE3, PTUBE4, PTUBE5, PTUBE6, PTUBE7, PTUBE8, PTUBE9, PTUBE10, PTUBE11, PTUBE12;
 
+
 // OFFSETS
+float PTUBE_CLB, STRAIN1_CLB, STRAIN2_CLB, STRAIN3_CLB, STRAIN4_CLB, STEER_ANG_CLB, TPS_CLB, F_BRK_PRES_CLB, B_BRK_PRES_CLB, FL_SUS_POT_CLB, FR_SUS_POT_CLB, BL_SUS_POT_CLB, BR_SUS_POT_CLB;
                 
 //FUNCTIONS
 
@@ -114,75 +116,78 @@ void digitalSensors(){
   if (FR_VSS == 0){
       diff = currentTime - FR_VSS_LastRead;
       wheelSpeed = wheelCirc/diff;
-      allSensors[2] = wheelSpeed;
+      allSensors[1] = wheelSpeed;
       FR_VSS_LastRead = currentTime;
   }
   // REPEAT WITH BACK WHEEL SPEEDS FROM MOTEC
   if (BL_VSS == 0){
       diff = currentTime - BL_VSS_LastRead;
       wheelSpeed = wheelCirc/diff;
-      allSensors[3] = wheelSpeed;
+      allSensors[2] = wheelSpeed;
       BL_VSS_LastRead = currentTime;
   }
   if (BR_VSS == 0){
       diff = currentTime - BR_VSS_LastRead;
       wheelSpeed = wheelCirc/diff;
-      allSensors[4] = wheelSpeed;
+      allSensors[3] = wheelSpeed;
       BR_VSS_LastRead = currentTime;
   }
 }
 
-
+int convertSensor(int sensorValue, int calibration=0);
 // sensor value from 0 to 2^16 and returns a voltage between 0 and 5 V
-int convertSensor(int sensorValue){
-  sensorVoltage = sensorValue * (systemVoltage/resolution);
+int convertSensor(int sensorValue, int calibration=0){
+  sensorVoltage = (sensorValue * (systemVoltage/resolution)) - calibration;
   return sensorVoltage;
 }
 
 void analogSensors(){
   
-  allSensors[5] = (convertSensor(analogRead(FL_BRK_TMP_PIN)) - 0.50)/0.005;
-  allSensors[6] = (convertSensor(analogRead(FR_BRK_TMP_PIN)) - 0.50)/0.005;
-  //  allSensors[7] = back l brake temp
-  //  allSensors[8] = back r brake temp
-  allSensors[9] = convertSensor(analogRead(FL_SUS_POT_PIN));
-  allSensors[10] = convertSensor(analogRead(FR_SUS_POT_PIN));
-  //  allSensors[11] = back l suspot
-  //  allSensors[12] = back r suspot
-  allSensors[13] = (convertSensor(analogRead(F_BRK_PRES_PIN)) - 0.50)/ (4/7500);
-  //  allSensors[14] = back brk pres
-  allSensors[15] = convertSensor(analogRead(STEER_ANG_PIN));
+  allSensors[4] = (convertSensor(analogRead(FL_BRK_TMP_PIN)) - 0.50)/0.005;
+  allSensors[5] = (convertSensor(analogRead(FR_BRK_TMP_PIN)) - 0.50)/0.005;
+  //  allSensors[6] = back l brake temp
+  //  allSensors[7] = back r brake temp
+  allSensors[8] = convertSensor(analogRead(FL_SUS_POT_PIN), FL_SUS_POT_CLB);
+  allSensors[9] = convertSensor(analogRead(FR_SUS_POT_PIN), FR_SUS_POT_CLB);
+  //  allSensors[10] = back l suspot
+  //  allSensors[11] = back r suspot
+  allSensors[12] = (convertSensor(analogRead(F_BRK_PRES_PIN), F_BRK_PRES_CLB) - 0.50)/ (4/7500);
+  //  allSensors[13] = back brk pres
+  allSensors[14] = convertSensor(analogRead(STEER_ANG_PIN), STEER_ANG_CLB);
   // motec
-  //  allSensors[16] = TPS
-  //  allSensors[17] = OIL_PRES
-  //  allSensors[18] = OIL_TEMP
-  //  allSensors[19] = COOL_TEMP
-  //  allSensors[20] = MAP
-  //  allSensors[21] = MAT
-  //  allSensors[22] = NEUT
-  //  allSensors[23] = LAMBDA1
-  //  allSensors[24] = LAMBDA2
+  //  allSensors[15] = TPS
+  //  allSensors[16] = OIL_PRES
+  //  allSensors[17] = OIL_TEMP
+  //  allSensors[18] = COOL_TEMP
+  //  allSensors[19] = MAP
+  //  allSensors[20] = MAT
+  //  allSensors[21] = NEUT
+  //  allSensors[22] = LAMBDA1
+  //  allSensors[23] = LAMBDA2
 
   // i2c
-  //  allSensors[25] = accel;
-  //  allSensors[26] = gyro;
-  //  allSensors[27] = gps;
-  allSensors[28] = convertSensor(ads1115b.readADC_Differential_0_1());
-  allSensors[29] = convertSensor(ads1115b.readADC_Differential_2_3());
-  allSensors[30] = convertSensor(ads1115c.readADC_Differential_0_1());
-  allSensors[31] = convertSensor(ads1115c.readADC_Differential_2_3());
-  allSensors[32] = convertSensor(analogRead(PTUBE1_PIN));
-  allSensors[33] = convertSensor(analogRead(PTUBE2_PIN));
-  allSensors[34] = convertSensor(analogRead(PTUBE3_PIN));
-  allSensors[35] = convertSensor(analogRead(PTUBE4_PIN));
-  allSensors[36] = convertSensor(analogRead(PTUBE5_PIN));
-  allSensors[37] = convertSensor(analogRead(PTUBE6_PIN));
-  allSensors[38] = convertSensor(analogRead(PTUBE7_PIN));
-  allSensors[39] = convertSensor(analogRead(PTUBE8_PIN));
-  allSensors[40] = convertSensor(analogRead(PTUBE9_PIN));
-  allSensors[41] = convertSensor(analogRead(PTUBE10_PIN));
+  //  allSensors[24] = accel;
+  //  allSensors[25] = gyro;
+  //  allSensors[26] = gps;
+
+  // strain gauges
+  allSensors[27] = convertSensor(ads1115b.readADC_Differential_0_1(), STRAIN1_CLB);
+  allSensors[28] = convertSensor(ads1115b.readADC_Differential_2_3(), STRAIN2_CLB);
+  allSensors[29] = convertSensor(ads1115c.readADC_Differential_0_1(), STRAIN3_CLB);
+  allSensors[30] = convertSensor(ads1115c.readADC_Differential_2_3(), STRAIN4_CLB);
+
+  allSensors[31] = convertSensor(analogRead(PTUBE1_PIN), PTUBE_CLB);
+  allSensors[32] = convertSensor(analogRead(PTUBE2_PIN));
+  allSensors[33] = convertSensor(analogRead(PTUBE3_PIN));
+  allSensors[34] = convertSensor(analogRead(PTUBE4_PIN));
+  allSensors[35] = convertSensor(analogRead(PTUBE5_PIN));
+  allSensors[36] = convertSensor(analogRead(PTUBE6_PIN));
+  allSensors[37] = convertSensor(analogRead(PTUBE7_PIN));
+  allSensors[38] = convertSensor(analogRead(PTUBE8_PIN));
+  allSensors[39] = convertSensor(analogRead(PTUBE9_PIN));
+  allSensors[40] = convertSensor(analogRead(PTUBE10_PIN));
+  allSensors[41] = convertSensor(ads1115a.readADC_SingleEnded(0));
   allSensors[42] = convertSensor(ads1115a.readADC_SingleEnded(0));
-  allSensors[43] = convertSensor(ads1115a.readADC_SingleEnded(0));
 }
 // saves to sd card
 void saveData(){
@@ -204,7 +209,7 @@ void compileCurData(){
   // convert to CSV
   dataString = "";
   for (int i = 0; i < sizeof(allSensors)/sizeof(int); i = i + 1) {
-    dataString = dataString + "," + String(allSensors[i]);
+    dataString = String(allSensors[i]) + " ,";
   } 
 }
 
@@ -239,6 +244,19 @@ void setup() {
   sensorDataVer.write(dataVer+1);
   sensorDataVer.close();
   fileName = "data" + String(dataVer);
+
+  // write headers
+  if (SD.exists(fileName)) { // check the card is still there
+    sensorData = SD.open(fileName, FILE_WRITE);
+    if (sensorData){
+      sensorData.println("FL_VSS,FR_VSS,BL_VSS,BR_VSS,FL_BRK_TMP,FR_BRK_TMP,BL_BRK_TMP,BR_BRK_TMP,FL_SUS_POT,FR_SUS_POT,BL_SUS_POT,BR_SUS_POT,F_BRK_PRES,B_BRK_PRES,STEER_ANG,TPS,OIL_PRES,OIL_TEMP,COOL_TEMP,MAP,MAT,NEUT,LAMBDA1,LAMBDA2,ACCEL,GYRO,GPS,STRAIN1,STRAIN2,STRAIN3,STRAIN4,PTUBE1,PTUBE2,PTUBE3,PTUBE4,PTUBE5,PTUBE6,PTUBE7,PTUBE8,PTUBE9,PTUBE10,PTUBE11,PTUBE12");
+      sensorData.close(); // close the file
+    }
+  } else {
+    Serial.println("Error writing to file !");
+  }
+
+  // set calibration variables
 
 }
 
