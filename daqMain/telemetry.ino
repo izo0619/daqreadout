f53e73b79676ce0e2a86b8c3c31ac44faff043aa
Dishen Wang <dishenwang1021@gmail.com>
2021-02-27 12:28:20 -0600

    adding telemetry

--- daqMain/telemetry.ino
+++ daqMain/telemetry.ino
@@ -1,12 +1,12 @@
 // writes sensor values to xbee
 void writeXbee(){
-  const int len = 36;
+  // excluding strain gauges and pitot tubes
+  const int len = 26;
   // significant digits of each sensor values
   int sensorSig[len] = {0, 0, 1, 2, 0, 0, 1, 2,
                         1, 1, 0, 0, 0, 0, 0, 0,
                         0, 0, 0, 3, 3, 3, 2, 1,
-                        0, 0, 0, 0, 0, 0, 0, 0,
-                        4, 1, 2, 1}; 
+                        0, 0}; 
   short shortSensorData[len];
   short id = -32767;
 
