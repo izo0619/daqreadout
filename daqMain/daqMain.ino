eb7036ed8964796364f29e6bdb7572fa0d42f55c
izo0619 <isabelzhong30@gmail.com>
2021-05-05 17:56:33 -0700

    commented out serialprints, abstracted read frequency

--- daqMain/daqMain.ino
+++ daqMain/daqMain.ino
@@ -36,6 +36,7 @@ const int CSpin = 10;
 String dataString =""; // holds the data to be written to the SD card
 String fileName;
 File sensorData;
+int readFrequency = 1000; // CHANGE THIS TO READ FASTER/SLOWER (units in ms)
 
 // leds for detecting sd card
 int led_r = 39;
@@ -132,7 +133,7 @@ void setup() {
 
   // see if the card is present and can be initialized
   if (!SD.begin(CSpin)) {
-  Serial.println("Card failed/not found");
+//  Serial.println("Card failed/not found");
   digitalWrite(led_r, LOW);
   digitalWrite(led_y, HIGH);
   digitalWrite(led_g, HIGH);
@@ -150,7 +151,7 @@ void setup() {
   if (sensorDataVer) {
         dataVer = sensorDataVer.read();
   } else {
-    Serial.println("File unavailable");
+    // Serial.println("File unavailable");
     digitalWrite(led_r, LOW);
     digitalWrite(led_y, LOW);
     digitalWrite(led_g, HIGH);
@@ -160,17 +161,15 @@ void setup() {
   sensorDataVer = SD.open("VerTrack.txt", O_RDWR);
   sensorDataVer.write(dataVer+1);
   sensorDataVer.close();
-  fileName = "data" + String(dataVer);
-
+  fileName = "data" + String(dataVer) + ".csv";
+ 
   // write headers
-  if (SD.exists(fileName)) { // check the card is still there
-    sensorData = SD.open(fileName, FILE_WRITE);
-    if (sensorData){
+  sensorData = SD.open(fileName, FILE_WRITE);
+  if (sensorData){
       sensorData.println("FL_VSS,FR_VSS,BL_VSS,BR_VSS,FL_BRK_TMP,FR_BRK_TMP,BL_BRK_TMP,BR_BRK_TMP,FL_SUS_POT,FR_SUS_POT,BL_SUS_POT,BR_SUS_POT,F_BRK_PRES,B_BRK_PRES,STEER_ANG,TPS,OIL_PRES,OIL_TEMP,COOL_TEMP,MAP,MAT,NEUT,LAMBDA1,LAMBDA2,ACCEL,GYRO,GPS,STRAIN1,STRAIN2,STRAIN3,STRAIN4,PTUBE1,PTUBE2,PTUBE3,PTUBE4,PTUBE5,PTUBE6,PTUBE7,PTUBE8,PTUBE9,PTUBE10,PTUBE11,PTUBE12");
       sensorData.close(); // close the file
-    }
-  } else {
-    Serial.println("Error writing to file !");
+    } else {
+    // Serial.println("Error writing to file !");
     digitalWrite(led_r, LOW);
     digitalWrite(led_y, LOW);
     digitalWrite(led_g, HIGH);
@@ -204,7 +203,7 @@ void loop() {
   digitalSensors();
 //  check for analog reading every second
 //  frequency of change of data
-  if (currentTime - previousTimeAnalog > 1000){
+  if (currentTime - previousTimeAnalog > readFrequency){
     previousTimeAnalog = currentTime;
     compileCurData();
     saveData();
